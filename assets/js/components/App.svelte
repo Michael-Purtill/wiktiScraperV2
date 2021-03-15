88259af7167312a189bdef986faf853b49029764
Michael <michaelpurtill123@hotmail.com>
2021-03-15 01:19:53 -0400

    lodash stuff

--- assets/js/components/App.svelte
+++ assets/js/components/App.svelte
@@ -1,1 +1,17 @@
-<p>Hello from Svelte!!!!</p>
+<script>
+    import * as ld from 'lodash';
+    import { onMount } from 'svelte';
+    
+    
+    let fileNames = [];
+    onMount(async () => {
+        let langList = [];
+        fetch("/api/langlist").then((r) => {return r.json()}).then((d) => {langList = d.langs })
+    })
+        
+    </script>
+    
+    <div>
+        <h3>Please Select a Language</h3>
+        
+    </div>
