5125abcf84033b4d8ea798d12269d27aade84b45
Michael <michaelpurtill123@hotmail.com>
2021-03-15 01:25:53 -0400

    language select displaying in svelte

--- assets/js/components/App.svelte
+++ assets/js/components/App.svelte
@@ -1,17 +1,23 @@
 <script>
-    import * as ld from 'lodash';
+    import * as _ from 'lodash';
     import { onMount } from 'svelte';
     
     
-    let fileNames = [];
+    let langList = [];
     onMount(async () => {
-        let langList = [];
-        fetch("/api/langlist").then((r) => {return r.json()}).then((d) => {langList = d.langs })
+        fetch("/api/langlist").then((r) => {return r.json()}).then((d) => {langList = _.filter(d.langs, (l) => l != "") })
     })
         
     </script>
     
     <div>
         <h3>Please Select a Language</h3>
-        
+        <div>
+            <select>
+                {#each langList as lang}
+                <option value={lang}>{lang}</option>
+                {/each}
+            </select>
+
+        </div>
     </div>
