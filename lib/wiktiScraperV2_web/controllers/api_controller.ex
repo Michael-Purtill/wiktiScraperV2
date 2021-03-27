323c9de40c8cf6ed8b3a87e06fc3c0c737a2df2d
Michael <michaelpurtill123@hotmail.com>
2021-03-27 01:56:35 -0400

    add a pos link finder

--- lib/wiktiScraperV2_web/controllers/api_controller.ex
+++ lib/wiktiScraperV2_web/controllers/api_controller.ex
@@ -17,10 +17,11 @@ defmodule WiktiScraperV2Web.ApiController do
           Floki.text(Enum.at(tds, 1))
         end
 
+        #return
         Enum.map(trs, build_lang_list)
 
       {:ok, %HTTPoison.Response{status_code: 404}} ->
-        IO.puts "Not found :("
+        IO.puts "Error trying to fetch language list html"
       {:error, %HTTPoison.Error{reason: _reason}} ->
         IO.puts "Error trying to fetch language list html"
     end
@@ -30,5 +31,28 @@ defmodule WiktiScraperV2Web.ApiController do
 
   defp lang_2_lemma_link(lang) do
     "https://en.wiktionary.org/wiki/Category:" <> lang <> "_lemmas"
+  end
+
+  defp lemma_link_2_pos_links(link) do
+    partsOfSpeech = case HTTPoison.get(link) do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        {:ok, document} = Floki.parse_document(body)
+        ul = Floki.find(document, "ul")
+        table = Enum.at(ul, 0)
+        lis = Floki.find(tbody, "li")
+
+        build_pos_list = fn li ->
+          a = Floki.find(li, "a")
+          Floki.attribute("href", Enum.at(a, 1)) #I think Floki.find always returns a array even if only 1 element is found, so need Enum.at
+        end
+
+        #return
+        Enum.map(a, build_pos_list)
+
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          IO.puts "Error trying to fetch language list html"
+        {:error, %HTTPoison.Error{reason: _reason}} ->
+          IO.puts "Error trying to fetch language list html"
+    end
   end
 end
